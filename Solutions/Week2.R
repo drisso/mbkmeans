76439caca495b46dee42ff5ade7c553ade360f5a
YuweiNi45 <yuweini45@gmail.com>
2018-07-16 10:02:26 -0400

    Add RcppArmadillo and Rcpp package

--- Solutions/Week2.R
+++ Solutions/Week2.R
@@ -19,7 +19,7 @@ cidx <- snow::splitIndices(ncol(data2), ncol(data2) / chunksize)
 
 lib.sizes <- n.exprs <- numeric(ncol(data2))
 tot.exprs <- numeric(nrow(data2))
-for (i in head(cidx, 2)) {
+for (i in cidx) {
   message(".", appendLF=FALSE)
   m <- as.matrix(counts(data2)[,i, drop=FALSE])
   lib.sizes[i] <- colSums(m)
@@ -29,21 +29,21 @@ for (i in head(cidx, 2)) {
 ave.exprs <- tot.exprs / ncol(data2)
 
 #calculate and save the library size, number of express genes and the average
-colData(data2)$lib.sizes <- lib.sizes
+#colData(data2)$lib.sizes <- lib.sizes
 colData(data2)$n.exprs <- n.exprs
 rowData(data2)$ave.exprs <- ave.exprs
 
 #filter the rows to remove the 0 counts
-feature<- rowSums(counts(data2)>0)>0
+feature<- DelayedArray::rowSums(counts(data2)>0)>0
 dataclean2<-data2[feature,]
 
 #counts for cell to be used a scaling
 library(scater)
 #dt<-colData(data2)
 #dataclean<- dt[which(dt[,"lib.sizes"]>0),]
-sizeFactors(data2)
-nor<-normalize(data2)
-assayNames(nor)
+sizeFactors(dataclean2) <- lib.sizes
+#nor<-normalize(dataclean2) ## this does not work... bug?
+#assayNames(nor)
 
 
 #Cluster
@@ -57,6 +57,8 @@ y1<-SingleCellExperiment(assays = list(counts=y))
 feature1<-rowSums(counts(y1)>3)>0
 y2<-y1[feature1,]
 counts(y2)
+
+saveRDS(dataclean2, file="filtered_data.rds")
 
 #Question:
 #1.Q about DropletUtils, see notes

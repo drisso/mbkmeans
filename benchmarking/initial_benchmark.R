8decb02032f73d6b736e9878db2309db4bdd019a
Davide Risso <dar2062@med.cornell.edu>
2018-08-27 10:41:39 -0400

    Initial benchmark

---
+++ benchmarking/initial_benchmark.R
@@ -0,0 +1,18 @@
+library(HDF5Array)
+library(pryr)
+devtools::load_all()
+
+mat <- HDF5Array("benchmarking/pbmc3k_rectangular.h5", name = "counts")
+
+data <- t(mat)[,1:1000]
+system.time(km <- mini_batch(data, clusters = 3, batch_size = 100, init_fraction = .1, max_iters = 10))
+system.time(clusters <- predict_mini_batch(data, CENTROIDS = km$centroids))
+
+data2 <- as.matrix(data)
+system.time(km <- MiniBatchKmeans(data2, clusters = 3, batch_size = 100, init_fraction = .1, max_iters = 10))
+system.time(clusters <- predict_MBatchKMeans(data2, CENTROIDS = km$centroids))
+
+system.time(km <- stats::kmeans(data, centers = 3))
+
+object_size(data)
+object_size(data2)

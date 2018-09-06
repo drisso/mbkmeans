52418f3222841c5a6dfb87e01dee4cbfef68238d
YuweiNi45 <yuweini45@gmail.com>
2018-09-06 15:22:17 -0400

    Delete the useless part

--- R/predict2.R
+++ R/predict2.R
@@ -14,25 +14,8 @@
 #' @return  It returns a vector with the clusters
 #'
 #'
-#'
 
 
-
-
-
-mat <- HDF5Array("benchmarking/pbmc3k_rectangular.h5", name = "counts")
-
-data <- t(mat)[,1:1000]
-
-data<-as.matrix(1:30,nrow=10)
-
-predict2(data,block_size=1000L, clusters = 3, batch_size = 100, init_fraction = .1, max_iters = 10)
-
-clusters = 3
-block_size=5L
-batch_size = 3
-init_fraction = .1
-max_iters = 4
 
 predict2<-function(data,block_size,clusters,batch_size,init_fraction,max_iters,initializer = "kmeans++"){
 
@@ -41,18 +24,13 @@ predict2<-function(data,block_size,clusters,batch_size,init_fraction,max_iters,i
   ##for each row, do predict_mini_batch
     km_block <- blockApply(
     x = data,
-    FUN = predict_mini_batch,
+    FUN = predict_MBatchKMeans,
     CENTROIDS = km$centroids,
     grid = RegularArrayGrid(
       refdim = dim(data),
       spacings = c(block_size, ncol(data))))
 
-    clean_data<-data.frame(matrix(unlist(km_block),nrow = block_size))
+    clean_data<-unlist(km_block)
 
-    final_group<-numeric(block_size)
-    for(i in 1:nrow(clean_data)){
-      final_group[i]<-min(clean_data[i,])
-    }
-
-    final_group
+    clean_data
 }

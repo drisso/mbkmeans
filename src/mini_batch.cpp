969dcb6e14c1f06031b98e92a077ec7f26a02118
Davide Risso <risso.davide@gmail.com>
2019-02-11 18:10:23 +0100

    Fix bug in mini_batch()

--- src/mini_batch.cpp
+++ src/mini_batch.cpp
@@ -395,24 +395,15 @@ Rcpp::NumericVector compute_wcss(Rcpp::NumericVector clusters, Rcpp::NumericMatr
 //' @export
 // [[Rcpp::export]]
 Rcpp::List mini_batch(SEXP data, int clusters, int batch_size, int max_iters, int num_init = 1, double init_fraction = 1.0, std::string initializer = "kmeans++",
-
                       bool calc_wcss = false, int early_stop_iter = 10, bool verbose = false, Rcpp::Nullable<Rcpp::NumericMatrix> CENTROIDS = R_NilValue, double tol = 1e-4){
 
   ClustHeader clust_header;
 
-  //clust_header.set_seed(seed);             // R's RNG
-
-  // Rcpp::Environment base_env("package:base");
-
-  // Rcpp::Function set_seed_r = base_env["set.seed"];
-
-  // set_seed_r(seed);
-
   int dat_n_rows = get_nrow(data);    // use the template function in this file
 
-  //int dat_n_rows = data.n_rows;
-
-  if (clusters > dat_n_rows - 2 || clusters < 1) { Rcpp::stop("the number of clusters should be at most equal to nrow(data) - 2 and never less than 1"); }
+  if (clusters > dat_n_rows - 2 || clusters < 1) {
+      Rcpp::stop("the number of clusters should be at most equal to nrow(data) - 2 and never less than 1");
+  }
 
   bool flag = false;
 
@@ -449,19 +440,6 @@ Rcpp::List mini_batch(SEXP data, int clusters, int batch_size, int max_iters, in
 
       if(initializer== "kmeans++"){
 
-        // if(init_fraction==1.0){
-        //
-        //   //SEXP centroids_matrix = transfer_data(data);
-        //
-        //   //update_centroids = Rcpp::as<arma::mat>(centroids_matrix);
-        //
-        //   SEXP trans_data = transfer_data(data);
-        //
-        //   arma::mat final_data = Rcpp::as<arma::mat>(trans_data);
-        //
-        //   update_centroids = clust_header.kmeans_pp_init(final_data, clusters, false);
-        //
-        // }else
         if(init_fraction <= 1.0 && init_fraction >0.0){
 
           Rcpp::NumericMatrix tran_data;
@@ -474,15 +452,11 @@ Rcpp::List mini_batch(SEXP data, int clusters, int batch_size, int max_iters, in
 
              tran_data =  subset_matrix(final_matrix,init_fraction);
 
-            //return tran_data;
-
           }else if(matrix_type ==REALSXP){
 
             auto final_matrix=beachmat::create_numeric_matrix(data);
 
              tran_data = subset_matrix(final_matrix,init_fraction);
-
-            //return tran_data;
 
           }else{
 
@@ -494,8 +468,6 @@ Rcpp::List mini_batch(SEXP data, int clusters, int batch_size, int max_iters, in
 
           update_centroids = clust_header.kmeans_pp_init(final_data, clusters, false);
 
-          // SEXP centroids_matrix = shuffle_matrix(data,init_fraction = init_fraction);
-
         }else{
 
           Rcpp::stop("The value of fraction should be larger than 0 and not larger than 1");
@@ -505,10 +477,6 @@ Rcpp::List mini_batch(SEXP data, int clusters, int batch_size, int max_iters, in
       }//kmeans++ ends
 
       if(initializer == "random"){
-
-        //arma::uvec samp = arma::conv_to< arma::uvec >::from(sample_vec(clusters, 0, data.n_rows - 1, false));
-
-        //update_centroids = data.rows(samp);
 
         Rcpp::NumericMatrix tran_data_random;
 
@@ -520,15 +488,11 @@ Rcpp::List mini_batch(SEXP data, int clusters, int batch_size, int max_iters, in
 
            tran_data_random =  subset_matrix_random(final_matrix,clusters);
 
-          //return tran_data_random;
-
         }else if(matrix_type ==REALSXP){
 
           auto final_matrix=beachmat::create_numeric_matrix(data);
 
            tran_data_random = subset_matrix_random(final_matrix,clusters);
-
-          //return tran_data_random;
 
         }else{
 
@@ -537,8 +501,6 @@ Rcpp::List mini_batch(SEXP data, int clusters, int batch_size, int max_iters, in
         }
 
         update_centroids = Rcpp::as<arma::mat>(tran_data_random);
-
-        //update_centroids = kmeans_pp_init(final_data, clusters, false);
 
       }//random ends
 
@@ -560,7 +522,7 @@ Rcpp::List mini_batch(SEXP data, int clusters, int batch_size, int max_iters, in
 
     int count = 0;
 
-    int itera =0;
+    int itera = 0;
 
     for (int i = 0; i < max_iters; i++) {
 
@@ -587,7 +549,6 @@ Rcpp::List mini_batch(SEXP data, int clusters, int batch_size, int max_iters, in
         Rcpp::stop("The type of matrix is wrong");
 
       }
-
 
       arma::mat batch_data = Rcpp::as<arma::mat>(batch_data_choose);
 
@@ -628,6 +589,7 @@ Rcpp::List mini_batch(SEXP data, int clusters, int batch_size, int max_iters, in
       double calc_cost = arma::accu(total_SSE);
 
       if (verbose) { Rcpp::Rcout << "iteration: " << i + 1 << "  --> total WCSS: " << calc_cost << "  -->  squared norm: " << tmp_norm << std::endl; }
+//      if (verbose) { Rcpp::Rcout << "iteration: " << i + 1 << "  --> previous centroids: " << previous_centroids << "  -->  current centroids: " << update_centroids << std::endl; }
 
       count = i;
 
@@ -637,7 +599,7 @@ Rcpp::List mini_batch(SEXP data, int clusters, int batch_size, int max_iters, in
 
         increment_early_stop = 0;
 
-        output_centroids = update_centroids;                // assign end-centroids and SSE when WCSS is minimal
+        previous_centroids = update_centroids;                // assign end-centroids and SSE when WCSS is minimal
 
         output_SSE = total_SSE;
       }
@@ -658,7 +620,7 @@ Rcpp::List mini_batch(SEXP data, int clusters, int batch_size, int max_iters, in
          break;
       }
 
-      if(i == max_iters -1){
+      if(i == max_iters - 1){
 
           output_centroids = update_centroids;
 
@@ -672,9 +634,9 @@ Rcpp::List mini_batch(SEXP data, int clusters, int batch_size, int max_iters, in
       }
     }
 
-    if(itera -1 == max_iters -1){
+    if(itera == max_iters){
 
-        std::string message = "iterations failed to converge, and the number of iterations is" + std::to_string(itera);
+        std::string message = "iterations failed to converge, and the number of iterations is " + std::to_string(itera);
 
         Rcpp::warning(message);
     }
@@ -696,57 +658,19 @@ Rcpp::List mini_batch(SEXP data, int clusters, int batch_size, int max_iters, in
 
   }
 
-  //int exc = arma::as_scalar(flag_exception(end_init - 1));                        // print warning OR stop function only if duplicates OR NA's are present in relevant output centroids [ end_init - 1 ]
-
-  //if (exc > 0) {
-
-  //if (initializer == "quantile_init") {
-
-  //  std::string message = "the centroid matrix using 'quantile_init' as initializer contains duplicates for number of clusters equal to : " + std::to_string(clusters);
-
-  //  Rcpp::warning(message);}
-
-  //if (initializer == "optimal_init") {
-
-  //  std::string message = "The centroid matrix using 'optimal_init' as initializer contains NA's. Thus, the 'tol_optimal_init' parameter should be (probably) decreased for number of clusters equal to : " + std::to_string(clusters);
-
-  //  Rcpp::stop(message);
-  //}
-  //}
-
-
 
   Rcpp::NumericVector clusterfinal = predict_mini_batch(data, Rcpp::wrap(centers_out));
+  Rcpp::NumericVector wcss_final;
 
   if(calc_wcss == TRUE){
-
-      Rcpp::NumericVector wcss_final = compute_wcss(clusterfinal,Rcpp::wrap(centers_out),data);
-
-      Rcpp::NumericVector clusterfinal_2 = predict_mini_batch(data, Rcpp::wrap(centers_out));
-
-  //arma::rowvec CLUSTERS;
-
-  //Rcpp::NumericMatrix centers;
-
-  //CLUSTERS = predict_mini_batch(data,centers);
-
+      wcss_final = compute_wcss(clusterfinal,Rcpp::wrap(centers_out),data);
+  }
 
       return Rcpp::List::create(Rcpp::Named("centroids") = centers_out,
-                                //Rcpp::Named("WCSS_per_cluster") = bst_WCSS,
                                 Rcpp::Named("WCSS_per_cluster") = wcss_final,
                                 Rcpp::Named("best_initialization") = end_init,
                                 Rcpp::Named("iters_per_initialization") = iter_before_stop,
-                                Rcpp::Named("Clusters") = clusterfinal_2);
-
-  }else{
-
-      return Rcpp::List::create(Rcpp::Named("centroids") = centers_out,
-                                //Rcpp::Named("WCSS_per_cluster") = bst_WCSS,
-                                //Rcpp::Named("WCSS_per_cluster") = wcss_final,
-                                Rcpp::Named("best_initialization") = end_init,
-                                Rcpp::Named("iters_per_initialization") = iter_before_stop,
                                 Rcpp::Named("Clusters") = clusterfinal);
-  }
 
 }
 

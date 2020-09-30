d978979a158975fa636977f6137db2bb34e8ed3c
Davide Risso <risso.davide@gmail.com>
2020-09-30 11:36:15 +0200

    Import colAutoGrid function

--- R/compute_labels.R
+++ R/compute_labels.R
@@ -10,7 +10,7 @@
 #'
 #' @return a vector of cluster labels for each observation.
 #'
-#' @importFrom DelayedArray blockApply colSums
+#' @importFrom DelayedArray blockApply colSums colAutoGrid
 #' @importFrom BiocParallel SerialParam
 #' @export
 #'
@@ -24,7 +24,7 @@ predict_mini_batch_r <- function(data, centroids,
                                  BPPARAM=BiocParallel::SerialParam(),
                                  ...) {
     unlist(blockApply(data, all_labels, centroids = centroids,
-                      grid = colGrid(data),
+                      grid = colAutoGrid(data),
                       BPPARAM = BPPARAM, ...))
 }
 
